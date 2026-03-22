a6c0373172934adeacf90c60ebcfb9db0046fdc9
isaciqo <isaciqo.42@gmail.com.com>
2026-03-22 00:56:09 -0300

    fix: add correct url

--- js/config.js
+++ js/config.js
@@ -9,4 +9,4 @@
 
 window.API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:3000'
-  : 'https://newrpgmap.onrender.com';
+  : 'https://rpgmapserver.onrender.com';
